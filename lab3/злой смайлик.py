f055f5f020ac48428030f728036e13737486099a
Irina6666 <iricha.i@ya.ru>
2020-01-27 12:20:22 +0300

    update

---
+++ lab3/злой смайлик.py
@@ -0,0 +1,27 @@
+from graph import *
+
+penColor(1, 1, 0)
+brushColor(255,254,0)
+circle(200, 150, 120)
+
+brushColor("black")
+rectangle(130, 200, 270, 220)
+
+brushColor("red")
+circle(140, 110, 25)
+brushColor("black")
+circle(140, 110, 15)
+brushColor("black")
+polygon([(172, 105),(175,100),(70,50),(65,55),(172, 105)])
+
+brushColor("red")
+circle(260, 115, 22)
+brushColor("black")
+circle(260, 115, 12)
+
+
+
+
+
+
+run()  
